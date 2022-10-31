14853ee529b75020d85819d27d4b4ea749558e4d
Sighery <Sighery@users.noreply.github.com>
2022-10-31 17:06:23 +0100

    Update Clap to v4.0 and fix breaking changes
    The old v2 Clap version we were using had some vulnerabilities, so it
    was a good time to update the dependency. However, in this time
    there's been a few breaking changes so the code also needed some
    changes to use the new Clap derive API.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use std::fs;
 use std::path::Path;
 
+use clap::{Parser, Subcommand};
+
 use rustympkglib::pkgdata::PkgData;
 
-fn main() {
-    let matches = clap::App::new(clap::crate_name!())
-        .about(clap::crate_description!())
-        .version(clap::crate_version!())
-        .author(clap::crate_authors!())
-        .setting(clap::AppSettings::SubcommandRequiredElseHelp)
-        .arg(
-            clap::Arg::with_name("pkgbuild")
-                .short("p")
-                .value_name("file")
-                .help("Use an alternate build script")
-                .takes_value(true)
-                .default_value("PKGBUILD"),
-        )
-        .subcommand(
-            clap::SubCommand::with_name("json")
-                .about("Print the parsed JSON representation of the PKGBUILD"),
-        )
-        .get_matches();
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Arguments {
+    #[clap(short, long, default_value_t=String::from("PKGBUILD"))]
+    /// Use an alternate build script path
+    pkgbuild: String,
 
-    let source_path = Path::new(matches.value_of("pkgbuild").unwrap());
+    #[clap(subcommand)]
+    export: ExportType,
+}
+
+#[derive(Subcommand, Debug)]
+enum ExportType {
+    /// Export the parsed JSON representation of the PKGBUILD
+    Json,
+}
+
+fn main() {
+    let args = Arguments::parse();
+    let source_path = Path::new(&args.pkgbuild);
     let source_code = fs::read_to_string(source_path).unwrap();
 
-    if matches.subcommand_matches("json").is_some() {
-        let pkgdata = PkgData::from_source(&source_code).unwrap();
-        println!("{}", serde_json::to_string(&pkgdata).unwrap());
+    match args.export {
+        ExportType::Json {} => {
+            let pkgdata = PkgData::from_source(&source_code).unwrap();
+            println!("{}", serde_json::to_string(&pkgdata).unwrap());
+        }
     }
 }
